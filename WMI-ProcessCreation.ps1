5d193be5b761b2bb65fd2988a6d8a7147ff9adcd
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-05-18 21:21:20 -0400

    Initial Commit

---
+++ WMI-ProcessCreation.ps1
@@ -0,0 +1,4 @@
+﻿$test = 'SELECT * FROM __InstanceCreationEvent WITHIN 10 WHERE TargetInstance ISA "win32_process" AND TargetInstance.name like "%chrome%" '
+$action = {Write-host hello}
+Register-WmiEvent -Query $test -SourceIdentifier "Im" -Action $action
+
