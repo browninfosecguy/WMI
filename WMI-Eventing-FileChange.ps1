7db32f515b92079d650d2206d1b74a8e6334e5cd
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-07-24 17:18:16 -0400

    Initial Commit

---
+++ WMI-Eventing-FileChange.ps1
@@ -0,0 +1,10 @@
+﻿$test = 'SELECT * FROM __InstanceModificationEvent WITHIN 10 WHERE TargetInstance ISA "cim_datafile" AND TargetInstance.name="C:\\Users\\sunny\\Documents\\sunny\\password.txt" '
+$action = {Write-host hello}
+Register-WmiEvent -Query $test -SourceIdentifier "Im" -Action $action
+
+
+
+
+
+
+#Register-WMIEvent -Query "select * from __instancecreationevent within 10 where targetinstance ISA cim_datafile and targetinstance.extension='txt'" -action {write-host "hello"}
