bc15e0e6d0159a7b177ecd07758d34899d1188d8
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-07-24 19:27:39 -0400

    Initial Commit

---
+++ WMI-Eventing-Hook.ps1
@@ -0,0 +1,4 @@
+﻿$test = 'SELECT * FROM __InstanceModificationEvent WITHIN 10 WHERE TargetInstance ISA "cim_datafile" AND TargetInstance.name="C:\\Users\\sunny\\Documents\\sunny\\hook.txt" AND TargetInstance.lastmodified > TargetInstance.creationdate '
+$action = {New-EventLog –LogName Application –Source “Hook”;Write-EventLog –LogName Application –Source “Hook” –EntryType Information –EventID 1 –Message “Bait Taken”}
+Register-wmiEvent -Query $test -SourceIdentifier "Hook" -Action $action
+
