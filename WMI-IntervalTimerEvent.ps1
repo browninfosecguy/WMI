d0facdd8e46ea8d4171a78335168b94a2e735f79
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-08-21 15:20:33 -0400

    Initial Commit

---
+++ WMI-IntervalTimerEvent.ps1
@@ -0,0 +1,14 @@
+﻿<#
+To remove the instance use following 
+Remove-CimInstance -Query 'select * from __IntervalTimerInstruction where TimerId="MyTimerId"'
+
+Once done to unregister the event run the following Command 
+Unregister-Event Kity
+#>
+
+
+New-CimInstance -ClassName __IntervalTimerInstruction -Property @{TimerId="MyTimerId";
+IntervalBetweenEvents=[uint32]5000}
+$test = 'SELECT * FROM __TimerEvent where TimerId="MyTimerId"'
+$action = {write-host Cat}
+Register-WmiEvent -Query $test -SourceIdentifier "Kity" -Action $action
