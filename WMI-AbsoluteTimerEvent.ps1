d828c3b42b02c3d1db384a05eeb4a4ede8a5a7ce
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-08-22 17:02:40 -0400

    Initial commit

---
+++ WMI-AbsoluteTimerEvent.ps1
@@ -0,0 +1,13 @@
+﻿<#
+To remove the instance use following 
+Remove-CimInstance -Query 'select * from __IntervalTimerInstruction where TimerId="MyTimerId"'
+
+Once done to unregister the event run the following Command 
+Unregister-Event Kity
+#>
+
+
+New-CimInstance -ClassName __AbsoluteTimerInstruction -Property @{TimerId="MyTimerId";EventDateTime=[DateTime]"2018-08-22 17:00"}
+$test = 'SELECT * FROM __TimerEvent where TimerId="MyTimerId"'
+$action = {write-host Cat}
+Register-WmiEvent -Query $test -SourceIdentifier "Kity" -Action $action
