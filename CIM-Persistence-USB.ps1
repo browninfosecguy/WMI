aec7c281f56ba76a25ecc25406521bb5ac44c5aa
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-03-04 13:44:54 -0400

    Update CIM-Persistence-USB.ps1

--- CIM-Persistence-USB.ps1
+++ CIM-Persistence-USB.ps1
@@ -8,7 +8,7 @@ Author: @browninfosecguy
 Use Following commands to cleanup the Permenant Event subscription
 
 Get-WmiObject __EventFilter -namespace root\subscription -filter "name='myFilter'" | Remove-WmiObject
-Get-WmiObject NTEventLogEventConsumer -Namespace root\subscription -filter "name='FolderWriteLogging'" | Remove-WmiObject
+Get-WmiObject NTEventLogEventConsumer -Namespace root\subscription -filter "name='USBLogging'" | Remove-WmiObject
 Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription -Filter "Filter = ""__eventfilter.name='myFilter'""" | Remove-WmiObject
 
 #>
@@ -21,7 +21,7 @@ $WMIFilterInstance = New-CimInstance -ClassName __EventFilter -Namespace "root\s
                  Query=$WQLQuery
                 }
 
-$WMIEventConsumer = New-CimInstance -ClassName NTEventLogEventConsumer -Namespace "root\subscription" -Property  @{Name="FolderWriteLogging";
+$WMIEventConsumer = New-CimInstance -ClassName NTEventLogEventConsumer -Namespace "root\subscription" -Property  @{Name="USBLogging";
                         EventId = [uint32] 1; EventType = [uint32] 4; #EventType can have following values; Error 1, FailureAudit 16, Information 4, SuccesAudit 8, Warning 2
                         SourceName="PowerShell-Script-Log"; Category= [uint16] 1000 } #Category is never really used but can have any value and basically meant to provide more information about the event
 
