cb9a38264b8edadea20b1f62ec6a084d5a4dcd9c
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-03-06 16:09:58 -0400

    Update CIM-FileChange-EventSubscription.ps1

--- CIM-FileChange-EventSubscription.ps1
+++ CIM-FileChange-EventSubscription.ps1
@@ -29,22 +29,22 @@ if(!(Get-CimInstance -ClassName __EventFilter -namespace root\subscription -filt
                          Query=$WQLQuery
                     }
 
-        $WMIEventConsumer = New-CimInstance -ClassName NTEventLogEventConsumer -Namespace "root\subscription" -Property  @{Name="myConsumer";
+            $WMIEventConsumer = New-CimInstance -ClassName NTEventLogEventConsumer -Namespace "root\subscription" -Property  @{Name="myConsumer";
                                 EventId = [uint32] 1; EventType = [uint32] 4; #EventType can have following values; Error 1, FailureAudit 16, Information 4, SuccesAudit 8, Warning 2
                                 SourceName="PowerShell-Script-Log"; Category= [uint16] 1000 } #Category is never really used but can have any value and basically meant to provide more information about the event
 
 
-        $WMIWventBinding = New-CimInstance -ClassName __FilterToConsumerBinding -Namespace "root\subscription" -Property @{Filter = [Ref] $WMIFilterInstance;
+            $WMIWventBinding = New-CimInstance -ClassName __FilterToConsumerBinding -Namespace "root\subscription" -Property @{Filter = [Ref] $WMIFilterInstance;
                             Consumer = [Ref] $WMIEventConsumer
                             }
-    }
+        }
     catch {
 
             "Could not create permanent WMI Event subscription"
             Get-CimInstance -ClassName __EventFilter -namespace root\subscription -filter "name='myFilter'" | Remove-CimInstance
             Get-CimInstance -ClassName NTEventLogEventConsumer -Namespace root\subscription -filter "name='myConsumer'" | Remove-CimInstance
             Get-CimInstance -ClassName __FilterToConsumerBinding -Namespace root\subscription -Filter "Filter = ""__eventfilter.name='myFilter'""" | Remove-CimInstance
-    }
+         }
 }
 else
 {
